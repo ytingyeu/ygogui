c2424a5dac4d209f19c97ba48ed7c8072460f25b
Tingyeu Yang <ytingyeu@asu.edu>
2018-12-12 03:04:59 -0800

    fix typo

--- showProgress.js
+++ showProgress.js
@@ -4,21 +4,21 @@ let refBtnCancel = document.getElementById("btn-cancel");
 
 /* ISSUE:
  * the msg progressWindow.webContents.send('update-duration', [arg, ]) send
- * is never caught by pcRenderer.on('update-durations'...
+ * is never caught by pcRenderer.on('update-duration'...
  * but ipcRenderer.on('update-progress'... is good
 */
 
-/*
-ipcRenderer.on('update-durations', (event, arg) => {
-    console.log('update-durations');
+ipcRenderer.on('update-duration', (event, arg) => {
+    console.log('update-duration');
 });
-*/
+
 
 ipcRenderer.on('update-progress', (event, time, duration) => {
     //console.log(arg);
     let refShowProgress = document.getElementById('show-progress');
     refShowProgress.innerHTML = time + ' / ' + duration;
 });
+
 
 refBtnCancel.addEventListener('click', () => {
     if (confirm('Are you sure?')) {
